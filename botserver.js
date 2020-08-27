0e3d51f7f03e416fa6896a84f4ac232da72e414e
d-gallo <65927579+d-gallo@users.noreply.github.com>
2020-08-27 15:00:03 -0400

    Added prefix commands to bot
    !bb args-info & !bb fuckingmove & !bb fucking-move

--- botserver.js
+++ botserver.js
@@ -1,14 +1,34 @@
+// introducing env variables
 require('dotenv').config();
+// discordjs library
 const Discord = require('discord.js');
+// Get new Bot Client
 const bot = new Discord.Client();
+// Video / Audio encoder/decoder
 let ffmpeg = require('ffmpeg');
+
+// configuartion file import
+const {prefix} = require('./config.json');
+
 
 bot.on('ready', ()=> {
     console.info(`Logged in as ${bot.user.tag}`)
 });
 
 bot.on('message', async m=>{
-    if(m.member.voice.channel && (m.content === 'fucking move')){
+
+    if(!m.content.startsWith(prefix) || m.author.bot ) return;
+    const args = m.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    if(command==='args-info'){
+        if(!args.length){
+            return m.channel.send(`You didn't provide any arguements, ${m.author}`)
+        }
+        m.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+    // !bb fucking move 
+    if((command ===  'fucking-move' || command === 'fuckingmove') && m.member.voice.channel) {
         m.channel.send(`STOP RIGHT THERE CRIMINAL SCUM. NO ONE BREAKS THE LAW ON MY WATCH ${m.author}`)
         const connection = await m.member.voice.channel.join();
         const dispatcher = connection.play('clip1.mp3');
@@ -23,7 +43,7 @@ bot.on('message', async m=>{
             console.error(e);
             connection.disconnect();
         });
-    }
+    } 
 })
 
 
