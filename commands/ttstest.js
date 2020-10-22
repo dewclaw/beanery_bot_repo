e22f244b6d07a259a72915ad740d703a1befc590
Dominic Gallo <dewclaw@pop-os.localdomain>
2020-10-21 22:57:48 -0400

    removed TTS test command

--- commands/ttstest.js
+++ commands/ttstest.js
@@ -1,10 +1,10 @@
-module.exports = {
-    name:"ttscheck",
-    description: "debugging tts",
-    async execute(m,args) {
-        console.log("testing text to speach message")
-        m.channel.send("Test of text to speach", {
-            tts: true
-        });
-    }
-}
+// module.exports = {
+//     name:"ttscheck",
+//     description: "debugging tts",
+//     async execute(m,args) {
+//         console.log("testing text to speach message")
+//         m.channel.send("Test of text to speach", {
+//             tts: true
+//         });
+//     }
+// }
