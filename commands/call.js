6fe2a17e704c6689453e45258bc1981a1beeab49
Dominic Gallo <dgallo@Dominics-MacBook-Pro.local>
2020-12-05 16:31:57 -0500

    added new todo: add call feature utilizing twilio api.

---
+++ commands/call.js
@@ -0,0 +1,29 @@
+// imports
+
+const { execute } = require("./dadjoke")
+
+const headers = { 
+
+}
+/**
+ * 1. Pull phone number to call
+ * 2. Validate phone number
+ * 3. Place call from server
+ * 4. Send audio to channel
+ * 5. Record audio form users
+ * 6. Send audio to call
+ * 7. Wait for hangup or command to stop? 
+ */
+module.exports = { 
+    name: 'call',
+    description: 'Calls a selected phone number in the United States',
+    async execute(m, args) {
+        // Get phone number to call
+        // Place call from server
+        // Send audio from call to channel
+        // Record audio from users
+        // Send audio from users
+        // Listen for command to exit? 
+        
+    }
+}
